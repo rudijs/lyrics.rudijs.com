ead356986ff5384c6ae2b9d19c897a87a86d6847
Rudi Starcevic <rudi.starcevic@viseo.com>
2018-09-03 20:48:57 +0800

    Add lyrics lookup and render page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import { Provider } from "./context";
 import Navbar from "./components/layout/Navbar";
 import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
 
 class App extends Component {
   apiKey = process.env.REACT_APP_MUSIXMATCH_API_KEY;
@@ -18,6 +19,7 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={Index} />
+                <Route exact path="/lyrics/track/:id" component={Lyrics} />
               </Switch>
             </div>
           </React.Fragment>
