7cb6c17123681fd24e50e81cbc9f59da95a8a9cf
Rudi Starcevic <rudi.starcevic@viseo.com>
2018-09-04 13:31:34 +0800

    Add context dispatcher and search by track

--- src/context.js
+++ src/context.js
@@ -5,10 +5,24 @@ const Context = React.createContext();
 
 export const Consumer = Context.Consumer;
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_TRACKS":
+      return {
+        ...state,
+        track_list: action.payload,
+        heading: "Search Results"
+      };
+    default:
+      return state;
+  }
+};
+
 export class Provider extends Component {
   state = {
     track_list: [],
-    heading: "Top 10 Tracks"
+    heading: "Top 10 Tracks",
+    dispatch: action => this.setState(state => reducer(state, action))
   };
 
   apiKey = process.env.REACT_APP_MUSIXMATCH_API_KEY;
